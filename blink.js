8ca421d440eea1a1970b56a3b2237dcd29411459
Julian Bruyat <bruyat.ju@live.fr>
2020-01-10 15:37:19 +0100

    fix blink.js

--- blink.js
+++ blink.js
@@ -3,8 +3,8 @@ let five = require("johnny-five");
 let board = new five.Board();
 
 
-board.on("reader", function() {
-  let led = new file.Led(13);
-  led.blink(666);
+board.on("ready", function() {
+  let led = new five.Led(13);
+  led.blink(1500);
 });
 
