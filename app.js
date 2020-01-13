305acafaa2c988f1f15a69ef6ddffb8c69b9c266
Thomas Bourg <tbourg07@gmail.com>
2020-01-13 12:21:29 +0100

    npm init

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+app.get('/interface', function (req, res) {
+  res.sendFile('interface.html', {
+      root:'.'
+  })
+})
+app.post('/feedback', function (req, res) {
+  res.sendFile('interface.html')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
