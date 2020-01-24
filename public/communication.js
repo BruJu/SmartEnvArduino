0a36f6656358209dc70d405ff3e36914099c89e3
BOURG THOMAS p1500925 <thomas.bourg@etu.univ-lyon1.fr>
2020-01-24 16:38:50 +0100

    tableau client

--- public/communication.js
+++ public/communication.js
@@ -29,3 +29,42 @@ function changeAmbiance() {
         type: 'POST'
     })
 }
+
+function sendHistoryRequest() {
+    /*$.ajax({
+        url: 'request',
+        data: {
+            type: 'historyRequest'
+        },
+        type: 'POST'
+    }).done((data) => {*/
+        let sample = {
+            'Blood,0,0,0': {
+                red: 800,
+                green: 160,
+                blue: 40
+            }
+        };
+        let table = $('#history');
+        table.empty();
+        for(let index in sample) {
+            let [ambiance, input_red, input_green, input_blue] = index.split(',');
+            let output_red = sample[index].red;
+            let output_green = sample[index].green;
+            let output_blue = sample[index].blue;
+            let row = $('<tr>');
+            let ambiance_cell = $('<td>');
+            let ambiance_div = $('<div>');
+            ambiance_div.html(ambiance);
+            ambiance_cell.append(ambiance_div);
+            let input_cell = $('<td>');
+            input_cell.css('background-color', 'rgb(' + input_red + ',' + input_green + ',' + input_blue + ')');
+            let output_cell = $('<td>');
+            output_cell.css('background-color', `rgb(${output_red},${output_green},${output_blue})`);
+            row.append(ambiance_cell);
+            row.append(input_cell);
+            row.append(output_cell);
+            table.append(row);
+        }
+    //});
+}
